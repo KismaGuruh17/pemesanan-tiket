e781f3de014149764905a58aefdd16efc74cc710
AlfanMuhaimin <alfanmuhaimin10@gmail.com>
2023-06-08 13:57:20 +0700

    simple_anim.xml pada SplashScreen

--- app/src/main/java/com/example/pemesanantiket/SplashScreen.java
+++ app/src/main/java/com/example/pemesanantiket/SplashScreen.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +16,27 @@ import androidx.core.content.ContextCompat;
 
 public class SplashScreen extends AppCompatActivity {
 
+    TextView judul;
+
+    ImageView gambarSplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //menghilangkan ActionBar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
+
+        judul = findViewById(R.id.judul);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.simple_anim);
+
+        judul.setAnimation(animation);
+
+        gambarSplash = findViewById(R.id.gambarSplash);
+
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.simple_anim);
+
+        gambarSplash.setAnimation(animation1);
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -27,6 +45,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
-        }, 3000L); //3000 L = 3 detik
+        }, 3000L);
     }
 }
