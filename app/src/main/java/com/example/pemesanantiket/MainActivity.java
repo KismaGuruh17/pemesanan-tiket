bc2154f416e71ddac080e5afae1454209c5c59e8
KismaGuruh17 <kismaguruh17@gmail.com>
2023-06-08 13:33:01 +0700

    menghubungkan MainActivity ke activity_buy

--- app/src/main/java/com/example/pemesanantiket/MainActivity.java
+++ app/src/main/java/com/example/pemesanantiket/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.pemesanantiket;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPesawat, btnKapal, btnKereta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPesawat = findViewById(R.id.btnPesawat);
+        btnKapal = findViewById(R.id.btnKapal);
+        btnKereta = findViewById(R.id.btnKereta);
+
+        btnPesawat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BuyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnKapal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(MainActivity.this, BuyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnKereta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BuyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
